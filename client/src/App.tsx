841205815d4d88d9f03cee1a31be5266540578e9
Qrare <Metbezu@gmail.com>
2022-11-29 17:59:25 +0300

    routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {useTypedSelector} from "./hooks/useTypedSelector";
+import {Route, Routes} from "react-router-dom";
+import ProjectsPage from "./pages/ProjectPage/ProjectsPage";
+import TasksPage from "./pages/TasksPage/TasksPage";
+import './App.scss'
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
     const {users, error, loading} = useTypedSelector(state => state.user)
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+        <Navbar/>
+        <Routes>
+            <Route path={'projects'} element={<ProjectsPage/>}/>
+            <Route path={'projects/:projectId'} element={<TasksPage/>}/>
+            <Route path={'*'} element={<ProjectsPage/>}/>
+        </Routes>
+    </>
   );
 }
 
