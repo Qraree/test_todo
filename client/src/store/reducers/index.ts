7d4df41c25303f239bf8e2ffa81a0c16b1bf2991
Qrare <Metbezu@gmail.com>
2022-11-29 21:48:50 +0300

    modal reducer

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {userReducer} from "./userReducer";
+import {modalReducer} from "./modalReducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    modal: modalReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
