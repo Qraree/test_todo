2a924366c1c35cc4dd79b3d1a63ad86e5a5c8296
Qrare <Metbezu@gmail.com>
2022-12-01 21:52:04 +0300

    fix modal reducer

--- client/src/store/reducers/modalReducer.ts
+++ client/src/store/reducers/modalReducer.ts
@@ -1,8 +1,32 @@
 import {ModalAction, ModalActionTypes, ModalState, showModalAction} from "../../types/modal";
+import {DateTime} from "luxon";
 
 export const initialState = {
     showModal: false,
-    currentTask: {name: 'Task'}
+    currentTask: {
+        _id: '1',
+        name: 'Task',
+        description: 'Just a simple description for the task',
+        createdAt: DateTime.now(),
+        deadline: DateTime.now(),
+        priority: 1,
+        files: [{name: 'File.1', added: DateTime.now()}, {name: 'File.2', added: DateTime.now()}],
+        status: 'QUEUE',
+        checkList: [{
+            name: 'Call mom',
+            done: false
+        },
+            {
+                name: 'call DAd',
+                done: true
+            }],
+        projectId: '1',
+        comments: [
+            {content: 'Bla-bla-bla', replies: [{content: 'You okay?'}]},
+            {content: 'I will do this task!'}
+        ]
+
+    },
 }
 
 
@@ -16,7 +40,7 @@ export const modalReducer = (state = initialState, action: ModalAction): ModalSt
         case ModalActionTypes.HIDE_MODAL:
             return {
                 showModal: false,
-                currentTask: {name: ''}
+                currentTask: {...state.currentTask, name: ''}
             }
         default:
             return state
