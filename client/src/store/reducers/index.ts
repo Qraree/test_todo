c9bdb11d0d6a782b2970cc7dc2510c0122f4443f
Qrare <Metbezu@gmail.com>
2022-12-02 22:32:48 +0300

    redux change

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import {userReducer} from "./userReducer";
 import {modalReducer} from "./modalReducer";
+import {uiReducer} from "./uiReducer";
 
 export const rootReducer = combineReducers({
     user: userReducer,
     modal: modalReducer,
+    ui: uiReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
